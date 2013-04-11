cc73081110c761ca94ce60b88c0ae301850ac55a
Hirotaka Takahashi <hirotaka.takahashi@widsley.com>
2013-04-11 22:41:47 +0900

    initial commit

--- app/controllers/helos_controller.rb
+++ app/controllers/helos_controller.rb
@@ -5,5 +5,8 @@ class HelosController < ApplicationController
     @title = "sample layout"
     @header_content = "header of content"
     @msg = "This is the displayed massage"
+    #@helos = helo.all
+    
+    render :layout=>"helo"
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-Helo.create(titel:'Hello', content:'this is test!')
+#Helo.create(titel:'Hello', content:'this is test!')
